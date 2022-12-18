ad63a5442c7f7dc1b851b02b6c95602515af4cb8
Brandon Green <bmmgreen@gmail.com>
2022-12-18 09:47:31 -0600

    reverting MixinMinecraftServer_D

--- q_misc_util/src/main/java/qouteall/q_misc_util/mixin/dimension/MixinMinecraftServer_D.java
+++ q_misc_util/src/main/java/qouteall/q_misc_util/mixin/dimension/MixinMinecraftServer_D.java
@@ -4,18 +4,16 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.progress.ChunkProgressListener;
 import net.minecraft.world.level.levelgen.WorldGenSettings;
-import net.minecraft.world.level.levelgen.WorldOptions;
 import net.minecraft.world.level.storage.WorldData;
+import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import qouteall.q_misc_util.api.DimensionAPI;
 import qouteall.q_misc_util.dimension.DimensionIdManagement;
-import qouteall.q_misc_util.dimension.DynamicDimensionsImpl;
+import qouteall.q_misc_util.forge.events.ServerDimensionsLoadEvent;
 
 @Mixin(MinecraftServer.class)
 public abstract class MixinMinecraftServer_D {
@@ -30,11 +28,12 @@ public abstract class MixinMinecraftServer_D {
     private void onBeforeCreateWorlds(
         ChunkProgressListener worldGenerationProgressListener, CallbackInfo ci
     ) {
-        WorldOptions worldOptions = worldData.worldGenOptions();
+        WorldGenSettings generatorOptions = worldData.worldGenSettings();
         
         RegistryAccess registryManager = registryAccess();
-        
-        DimensionAPI.serverDimensionsLoadEvent.invoker().run(worldOptions, registryManager);
+
+        MinecraftForge.EVENT_BUS.post(new ServerDimensionsLoadEvent(generatorOptions, registryManager));
+        //DimensionAPI.serverDimensionsLoadEvent.invoker().run(generatorOptions, registryManager);
         
     }
     
