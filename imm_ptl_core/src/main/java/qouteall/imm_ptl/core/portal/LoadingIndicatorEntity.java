0d5ef95453d863b176e239639ae3ebadb5f4f13e
Brandon Green <bmmgreen@gmail.com>
2022-12-18 10:25:41 -0600

    reverting IPRegistry and LoadingIndicatorEntity

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/portal/LoadingIndicatorEntity.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/portal/LoadingIndicatorEntity.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -17,37 +16,39 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import qouteall.imm_ptl.core.CHelper;
-import qouteall.imm_ptl.core.platform_specific.IPNetworking;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.network.NetworkDirection;
+import qouteall.imm_ptl.core.platform_specific.IPRegistry;
+import qouteall.imm_ptl.core.platform_specific.forge.networking.IPMessage;
+import qouteall.imm_ptl.core.platform_specific.forge.networking.Spawn_Entity;
 import qouteall.imm_ptl.core.portal.nether_portal.BlockPortalShape;
 import qouteall.q_misc_util.my_util.IntBox;
 
-import java.util.Random;
-
 public class LoadingIndicatorEntity extends Entity {
-    public static EntityType<LoadingIndicatorEntity> entityType;
-    
+    public static EntityType<LoadingIndicatorEntity> entityType = IPRegistry.LOADING_INDICATOR.get();
+
     private static final EntityDataAccessor<Component> text = SynchedEntityData.defineId(
-        LoadingIndicatorEntity.class, EntityDataSerializers.COMPONENT
+            LoadingIndicatorEntity.class, EntityDataSerializers.COMPONENT
     );
-    
+
     public boolean isValid = false;
-    
+
     public BlockPortalShape portalShape;
-    
+
     public LoadingIndicatorEntity(EntityType type, Level world) {
         super(type, world);
     }
-    
+
     @Override
     public Iterable<ItemStack> getArmorSlots() {
         return null;
     }
-    
+
     @Override
     public void tick() {
         super.tick();
-        
+
         if (level.isClientSide()) {
             tickClient();
         }
@@ -58,86 +59,86 @@ public class LoadingIndicatorEntity extends Entity {
             }
         }
     }
-    
+
     @OnlyIn(Dist.CLIENT)
     private void tickClient() {
         addParticles();
-        
+
         if (tickCount > 40) {
             showMessageClient();
         }
     }
-    
+
     @OnlyIn(Dist.CLIENT)
     private void addParticles() {
         int num = tickCount < 100 ? 50 : 20;
-        
+
         if (portalShape != null) {
             IntBox box = portalShape.innerAreaBox;
             BlockPos size = box.getSize();
             RandomSource random = level.getRandom();
-            
+
             for (int i = 0; i < num; i++) {
                 Vec3 p = new Vec3(
-                    random.nextDouble(), random.nextDouble(), random.nextDouble()
+                        random.nextDouble(), random.nextDouble(), random.nextDouble()
                 ).multiply(Vec3.atLowerCornerOf(size)).add(Vec3.atLowerCornerOf(box.l));
-                
+
                 double speedMultiplier = 20;
-                
+
                 double vx = speedMultiplier * ((double) random.nextFloat() - 0.5D) * 0.5D;
                 double vy = speedMultiplier * ((double) random.nextFloat() - 0.5D) * 0.5D;
                 double vz = speedMultiplier * ((double) random.nextFloat() - 0.5D) * 0.5D;
-                
+
                 level.addParticle(
-                    ParticleTypes.PORTAL,
-                    p.x, p.y, p.z,
-                    vx, vy, vz
+                        ParticleTypes.PORTAL,
+                        p.x, p.y, p.z,
+                        vx, vy, vz
                 );
             }
         }
     }
-    
+
     @Override
     protected void defineSynchedData() {
         getEntityData().define(text, Component.literal("Loading..."));
     }
-    
+
     @Override
     protected void readAdditionalSaveData(CompoundTag tag) {
         if (tag.contains("shape")) {
             portalShape = new BlockPortalShape(tag.getCompound("shape"));
         }
     }
-    
+
     @Override
     protected void addAdditionalSaveData(CompoundTag tag) {
         if (portalShape != null) {
             tag.put("shape", portalShape.toTag());
         }
     }
-    
+
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket() {
-        return IPNetworking.createStcSpawnEntity(this);
+    public Packet<?> getAddEntityPacket() {
+        return IPMessage.INSTANCE.toVanillaPacket(new Spawn_Entity(this), NetworkDirection.PLAY_TO_CLIENT);
     }
-    
+
     public void inform(Component str) {
         setText(str);
     }
-    
+
     public void setText(Component str) {
         getEntityData().set(text, str);
     }
-    
+
     public Component getText() {
         return getEntityData().get(text);
     }
-    
+
     @OnlyIn(Dist.CLIENT)
     private void showMessageClient() {
         Gui inGameHud = Minecraft.getInstance().gui;
         inGameHud.setOverlayMessage(
-            getText(), false
+                getText(), false
         );
     }
 }
